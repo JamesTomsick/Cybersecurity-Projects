034e0bc8396edc91d2019f99013285d6c8137760
James Tomsick <jtomsick1258@gmail.com>
2026-07-23 14:59:55 -0400

    created my own code and then tried to do it without looking it up

--- Log-Analyzer/log_search_challenge.py
+++ Log-Analyzer/log_search_challenge.py
@@ -1,15 +1,14 @@
-log_file_name = input("Enter the log file name: ")
+file_name = input("Enter the log file name: ")
 search_term = input("Enter a search term: ").lower()
 match_count = 0
 try:
-    with open(log_file_name, "r") as log_file:
+    with open(file_name, "r" ) as log_file:
         for line in log_file:
             lowercase_line = line.lower()
-
             if search_term in lowercase_line:
                 print(line.strip())
                 match_count += 1
 
-    print(f"Total matches found: {match_count}")
+    print(f"total matches found: {match_count}")
 except FileNotFoundError:
-    print(f"Error: The file '{log_file_name}' was not found.")
+    print(f"Error the file {file_name} Not Found")
